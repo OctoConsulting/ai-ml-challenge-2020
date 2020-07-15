c6459f4ec76c0748be51a7e4e85aa80d988351fa
andreamycroft <andreamycroft@gmail.com>
2020-07-15 14:39:06 -0400

    Sentence parsing

---
+++ submissions/OctoConsulting_Submission/README.md
@@ -0,0 +1,1 @@
+This file contains the Octo Consulting submission for the GSA AI/ML Challenge
--- submissions/OctoConsulting_Submission/backend/clause_parsing.py
+++ submissions/OctoConsulting_Submission/backend/clause_parsing.py
@@ -1,2 +1,16 @@
-# Input: Body of Text
-# Output: Individual Clauses
+# The purpose of this file is to parse EULA documents into individual clauses
+
+import nltk
+
+
+def parse_clauses(body):
+    # Input: String of sentences
+    # Output: List of tokenized sentences
+    tokenizer = nltk.data.load('/punkt/english.pickle')
+    return tokenizer.tokenize(body)
+
+
+if __name__ == '__main__':
+    test_text = "There isn't time, so brief is life. And but an instant, so to speak, for that."
+    clauses = parse_clauses(test_text)
+    print(clauses)
