075c064c52c87f5f4839583fde8b9bbdd17a5f49
Meredith Lee <sneaksfile@gmail.com>
2020-08-07 10:07:07 -0400

    set up flask server to receive post requests

--- submissions/OctoConsulting_Submission/backend/flask_app.py
+++ submissions/OctoConsulting_Submission/backend/flask_app.py
@@ -1,11 +1,67 @@
-from flask import Flask
-import clause_parsing
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+# import clause_parsing
+
+#path to local upload_folder
+UPLOAD_FOLDER = 'C:/Users/meredith.lee/Documents/GitHub/GSA-AI/submissions/OctoConsulting_Submission/backend/testdata/uploads/'
+#Allows .pdf and .doc
+ALLOWED_EXTENSIONS = {'.pdf','.docx'}
 
 app = Flask(__name__)
+app.secret_key = 'some secret key'
+app.config['UPLOAD_FOLDER']= UPLOAD_FOLDER
 
-@app.route("/")
-def hello():
-    return clause_parsing.extract_all_clauses('hello.none')
+#Allowed file types
+def allowed_file(filename):
+    return 'true'
+    # return '.' in filename and \
+    #        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET','POST'])
+# def hello():
+#     return "Hello World"
+    # return clause_parsing.extract_all_clauses('hello.none')
+
+def upload_file():   
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        print(allowed_file(file.filename))    
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return  '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method="POST" enctype= "multipart/form-data">
+      <input type="file" name="file" />
+      <input type="submit" value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
 if __name__ == '__main__':
     app.run()
