73ee480ee25d23fd8d51768047e38875e8974050
Andrea Mycroft <38407944+andreamycroft@users.noreply.github.com>
2020-08-13 12:40:24 -0400

    Delete unacceptable_terms.py

--- submissions/OctoConsulting_Submission/backend/unacceptable_terms.py
+++
@@ -1,9 +0,0 @@
-unacceptable_terms = ['indemnity', 'indemnities', 'indemnify', 'indemnifies',
-                      'automatic renewal', 'automatic renewals', 'renew automatically', 'renews automatically',
-                      'tax', 'taxes',
-                      'third party', 'third-party', 'third parties', 'third-parties',
-                      'equitable remedy', 'equitable remedies', 'injunction', 'injunctions',
-                      'unilateral termination', 'termination'
-                      'unilateral modification', 'unilateral',
-                      'without notice',
-                      'derivative works', 'derivative work']
